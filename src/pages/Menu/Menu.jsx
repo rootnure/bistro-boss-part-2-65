69bad8d6dfdd1d8697cdbdeed269bb152cfe8e6d
rootnure <nur.diu.2791@gmail.com>
2023-11-14 19:14:44 +0600

    Menu Items load to UI upon category

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -7,9 +7,11 @@ import sectionBannerBgImg from "../../assets/home/chef-service.jpg";
 
 const Menu = () => {
     const [menu, loading] = useMenu();
-    const offered = menu.filter(item => item.category.toLowerCase() === "offered");
-    const dessert = menu.filter(item => item.category.toLowerCase() === "dessert");
-    // const salad = menu.filter(item => item.category.toLowerCase() === "salad");
+    const offers = menu.filter(item => item.category.toLowerCase() === "offered");
+    const desserts = menu.filter(item => item.category.toLowerCase() === "dessert");
+    const pizzas = menu.filter(item => item.category.toLowerCase() === "pizza");
+    const salads = menu.filter(item => item.category.toLowerCase() === "salad");
+    const soups = menu.filter(item => item.category.toLowerCase() === "soup");
     return (
         <>
             <Helmet>
@@ -17,6 +19,7 @@ const Menu = () => {
             </Helmet>
             <div className="space-y-24 mb-24">
                 <Cover
+                    isPageCover
                     bgImg={menuImg}
                     title="Menu"
                     description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur similique ex iure quo magnam magni quos velit inventore corporis dolorum?"
@@ -26,7 +29,7 @@ const Menu = () => {
                     sectionHeading="Today's Offer"
                     sectionSubHeading="Don't Miss"
                     loading={loading}
-                    items={offered}
+                    items={offers}
                     sectionBtnText="Order Your Favourate Food"
                 ></MenuItems>
                 {/* dessert menu */}
@@ -35,10 +38,36 @@ const Menu = () => {
                     sectionBannerTitle="Desserts"
                     sectionBannerDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta dolorem accusantium architecto voluptatum provident? Expedita beatae vero dignissimos sit dicta repellat, ad quaerat veritatis!"
                     loading={loading}
-                    items={dessert}
+                    items={desserts}
                     sectionBtnText="Order Your Favourate Food"
                 ></MenuItems>
-
+                {/* pizza menu */}
+                <MenuItems
+                    sectionBannerBgImgLink={sectionBannerBgImg}
+                    sectionBannerTitle="Pizza"
+                    sectionBannerDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta dolorem accusantium architecto voluptatum provident? Expedita beatae vero dignissimos sit dicta repellat, ad quaerat veritatis!"
+                    loading={loading}
+                    items={pizzas}
+                    sectionBtnText="Order Your Favourate Food"
+                ></MenuItems>
+                {/* salads menu */}
+                <MenuItems
+                    sectionBannerBgImgLink={sectionBannerBgImg}
+                    sectionBannerTitle="Salad"
+                    sectionBannerDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta dolorem accusantium architecto voluptatum provident? Expedita beatae vero dignissimos sit dicta repellat, ad quaerat veritatis!"
+                    loading={loading}
+                    items={salads}
+                    sectionBtnText="Order Your Favourate Food"
+                ></MenuItems>
+                {/* soups menu */}
+                <MenuItems
+                    sectionBannerBgImgLink={sectionBannerBgImg}
+                    sectionBannerTitle="Soups"
+                    sectionBannerDescription="Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe soluta dolorem accusantium architecto voluptatum provident? Expedita beatae vero dignissimos sit dicta repellat, ad quaerat veritatis!"
+                    loading={loading}
+                    items={soups}
+                    sectionBtnText="Order Your Favourate Food"
+                ></MenuItems>
             </div>
         </>
     );
--- src/pages/Shared/Cover.jsx
+++ src/pages/Shared/Cover.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Parallax } from 'react-parallax';
 
-const Cover = ({ bgImg, title, description }) => {
+const Cover = ({ bgImg, title, description, isPageCover = false }) => {
     return (
         <>
             {(title || description || bgImg) ?
@@ -11,12 +11,12 @@ const Cover = ({ bgImg, title, description }) => {
                     bgImageAlt={`Background Image for ${title}`}
                     strength={-200}
                 >
-                    <section className={`hero min-h-fit md:h-[700px] px-8 md:px-16 lg:px-40 py-12 md:py-28`} >
+                    <section className={`hero min-h-fit ${isPageCover ? "md:h-[850px]" : "md:h-[700px]"} px-8 md:px-16 lg:px-40 py-12 md:py-28`} >
                         <div className="hero-overlay bg-opacity-70 bg-black"></div>
                         <div className="hero-content text-center p-6 md:p-12 lg:p-20 text-white">
                             <div className="mb-4">
-                                {title ?
-                                    <h1 className="mb-5 text-5xl font-bold">{title}</h1> : ""}
+                                {title ? isPageCover ?
+                                    <h1 className="mb-6 text-7xl font-bold uppercase">{title}</h1> : <h1 className="mb-5 text-5xl font-bold uppercase"><span className="text-6xl">{title[0]}</span>{title.slice(1, title.length)}</h1> : ""}
                                 {description ?
                                     <p>{description}</p> : ""}
                             </div>
@@ -28,6 +28,7 @@ const Cover = ({ bgImg, title, description }) => {
 };
 
 Cover.propTypes = {
+    isPageCover: PropTypes.bool,
     bgImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired
